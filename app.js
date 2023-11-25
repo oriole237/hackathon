e5e4d3e5fcfab3b59b7cb6783386f8387cfe1167
oriole237 <eoriole98@gmail.com>
2023-11-25 17:24:49 +0100

    screen reader functionnality on menu-itemé

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 // Your javascript goes here
+
+var user_menu = document.getElementById("user-menu");
+var user_content = document.getElementById("user-menu-content");
+
+//hide show davii collection content
+user_content.addEventListener('click', function(){ alert("in")
+    user_content.toggle();
+});
+
 function RemoveBoxTrial(){
     var box = document.getElementById("select_plan");
     box.style.display = "none";
@@ -8,3 +17,4 @@ console.log("fd");
     var box = document.getElementById("dropdown_notification");
     box.style.display = "block";
 }
+
