8cea5bcc6066cbb7457fb2e1a634dbf9edbc41bc
oriole237 <eoriole98@gmail.com>
2023-11-25 12:56:15 +0100

    working on select plan

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // Your javascript goes here
+
+let span_close = document.getElementById("span_close");
+
+// mask trial plan message
+span_close.onclick = function(){
+    let trial_msg = document.getElementById("trial_msg");
+    trial_msg.style.display = "none";
+}
